4a9b7fd3764266edbd8a5aed4ab827d134ae06a6
Michal Jurkian <michal@kukidigital.com>
2019-05-11 12:57:05 +0100

    ESlinit added

--- src/Accordion.js
+++ src/Accordion.js
@@ -2,157 +2,151 @@ import './polyfill/arrayFrom';
 
 const KukiAccordion = (() => {
 
-    /* =========== variables =========== */
+  /* =========== variables =========== */
 
-    let Accordion;
-    let Panel;
+  let Accordion;
+  let Panel;
+  let options;
 
-    let options;
+  /* =========== private methods =========== */
 
-    /* =========== private methods =========== */
+  function cacheDOM() {
+    const { accordionClass, accordionContentClass } = options;
+    Accordion = document.querySelectorAll('.' + accordionClass);
+    Panel = document.querySelectorAll('.' + accordionContentClass);
+  }
 
-    function cacheDOM() {
-        const {accordionClass, accordionContentClass} = options;
-        Accordion = document.querySelectorAll('.' + accordionClass);
-        Panel = document.querySelectorAll('.' + accordionContentClass);
+  /**
+   * @param element {Element}
+   */
+  function openAccordion(element) {
+    const { activeClass, speed, easingOption, accordionStructure, duration } = options;
+    element.classList.add(activeClass);
+    let panel;
+
+    if (accordionStructure === 'nested') {
+      panel = element.lastElementChild;
+    } else {
+      panel = element.nextElementSibling;
     }
 
-    function setupEventListeners() {
-        for (let i = 0; i < Accordion.length; i++) {
-            Accordion[i].addEventListener('click', function (event) {
-                event.stopPropagation();
-                event.preventDefault();
-                onClick(event);
-            });
-        }
-    }
+    const panelHeight = panel.scrollHeight;
 
-    function noJS() {
-        const {accordionStructure} = options;
-        let panelOverflow;
-        for (let i = 0; i < Accordion.length; i++) {
+    const keyframeDuration = duration ? duration : (panelHeight / 100) * speed;
 
-            if (accordionStructure === 'nested') {
-                panelOverflow = Accordion[i].lastElementChild;
-            } else {
-                panelOverflow = Accordion[i].nextElementSibling;
-            }
-
-            panelOverflow.style.overflow = 'hidden';
-
-            closeAccordion(Accordion[i]);
-        }
-    }
-
-    function onClick(event) {
-        event.preventDefault();
-        const {accordionClass, activeClass} = options;
-
-        let targetClicked = event.target;
-        let classClicked = targetClicked.classList;
-
-        while ((classClicked[0] !== accordionClass)) {
-            targetClicked = targetClicked.parentElement;
-            classClicked = targetClicked.classList;
-        }
-
-        let targetClass = targetClicked.classList;
-
-        if (targetClass.value === accordionClass + ' ' + activeClass) {
-            closeAccordion(targetClicked);
-        } else {
-
-            for (let i = 0; i < Accordion.length; i++) {
-
-                const classList = Array.from(Accordion[i].classList);
-
-                if (classList.indexOf(activeClass) !== -1) {
-                    closeAccordion(Accordion[i]);
-                }
-            }
-            openAccordion(targetClicked);
-        }
-    }
-
-    /**
-     * @param element {Element}
-     */
-    function openAccordion(element) {
-        const {activeClass, speed, easingOption, accordionStructure, duration} = options;
-        element.classList.add(activeClass);
-        let panel;
-
-        if (accordionStructure === 'nested') {
-            panel = element.lastElementChild;
-        } else {
-            panel = element.nextElementSibling;
-        }
-
-        let panelHeight = panel.scrollHeight;
-
-        const keyframeDuration = duration ? duration : (panelHeight / 100) * speed;
-
-        panel.setAttribute('style', `
+    panel.setAttribute('style', `
             overflow: hidden;
             transition: height ${keyframeDuration}ms ${easingOption};
             height: ${panelHeight}px;
         `);
 
+  }
+
+  /**
+   * @param element {Element}
+   */
+  function closeAccordion(element) {
+    const { activeClass, speed, easingOption, accordionStructure, duration } = options;
+    let panel;
+
+    if (accordionStructure === 'nested') {
+      panel = element.lastElementChild;
+    } else {
+      panel = element.nextElementSibling;
     }
 
-    /**
-     * @param element {Element}
-     */
-    function closeAccordion(element) {
-        const {activeClass, speed, easingOption, accordionStructure, duration} = options;
-        let panel;
+    let panelHeight = getComputedStyle(panel).height;
+    panelHeight = panelHeight.substr(0, panelHeight.length - 2);
 
-        if (accordionStructure === 'nested') {
-            panel = element.lastElementChild;
-        } else {
-            panel = element.nextElementSibling;
-        }
+    const keyframeDuration = duration ? duration : (panelHeight / 100) * speed;
 
-        let panelHeight = getComputedStyle(panel).height;
-        panelHeight = panelHeight.substr(0, panelHeight.length - 2);
+    element.classList.remove(activeClass);
 
-        const keyframeDuration = duration ? duration : (panelHeight / 100) * speed;
-
-        element.classList.remove(activeClass);
-
-        panel.setAttribute('style', `
+    panel.setAttribute('style', `
             overflow: hidden;
             transition: height ${keyframeDuration}ms ${easingOption};
             height: 0px;
         `);
+  }
 
+  function onClick(event) {
+    event.preventDefault();
+    const { accordionClass, activeClass } = options;
+
+    let targetClicked = event.target;
+    let classClicked = targetClicked.classList;
+
+    while ((classClicked[0] !== accordionClass)) {
+      targetClicked = targetClicked.parentElement;
+      classClicked = targetClicked.classList;
     }
 
-    /* =========== public methods =========== */
+    const targetClass = targetClicked.classList;
 
-    function init(customOptions) {
+    if (targetClass.value === accordionClass + ' ' + activeClass) {
+      closeAccordion(targetClicked);
+    } else {
+      for (let i = 0; i < Accordion.length; i += 1) {
+        const classList = Array.from(Accordion[i].classList);
 
-        const defaults = {
-            accordionClass: 'accordion', // Accepts any string
-            accordionContentClass: 'panel', // Accepts any string
-            activeClass: 'active', // Accepts any string
-            accordionStructure: 'paired', // string 'paired' or 'nested'
-            easingOption: 'ease-in-out', // Accepts string https://developer.mozilla.org/en-US/docs/Web/API/EffectTiming/easing,
-            duration: false, // {boolean/number} if false, then we use speed to dictate animation duration, if a number is supplied, then we use this as the duration for ALL accordions, regardless of height
-            speed: 500 // Number - Speed of the animation in ms
-        };
-
-        options = Object.assign(defaults, customOptions);
-
-        cacheDOM();
-        setupEventListeners();
-        noJS();
+        if (classList.indexOf(activeClass) !== -1) {
+          closeAccordion(Accordion[i]);
+        }
+      }
+      openAccordion(targetClicked);
     }
+  }
 
-    /* =========== export public methods and variables =========== */
+  function setupEventListeners() {
+    for (let i = 0; i < Accordion.length; i += 1) {
+      Accordion[i].addEventListener('click', (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        onClick(event);
+      });
+    }
+  }
 
-    return {init};
+  function noJS() {
+    const { accordionStructure } = options;
+    let panelOverflow;
+    for (let i = 0; i < Accordion.length; i += 1) {
+      if (accordionStructure === 'nested') {
+        panelOverflow = Accordion[i].lastElementChild;
+      } else {
+        panelOverflow = Accordion[i].nextElementSibling;
+      }
 
+      panelOverflow.style.overflow = 'hidden';
+
+      closeAccordion(Accordion[i]);
+    }
+  }
+
+
+  /* =========== public methods =========== */
+
+  function init(customOptions) {
+    const defaults = {
+      accordionClass: 'accordion', // Accepts any string
+      accordionContentClass: 'panel', // Accepts any string
+      activeClass: 'active', // Accepts any string
+      accordionStructure: 'paired', // string 'paired' or 'nested'
+      easingOption: 'ease-in-out', // Accepts string https://developer.mozilla.org/en-US/docs/Web/API/EffectTiming/easing,
+      duration: false, // {boolean/number}
+      speed: 500 // Number - Speed of the animation in ms
+    };
+
+    options = Object.assign(defaults, customOptions);
+
+    cacheDOM();
+    setupEventListeners();
+    noJS();
+  }
+
+  /* =========== export public methods and variables =========== */
+
+  return { init };
 })();
 
 export default KukiAccordion;
